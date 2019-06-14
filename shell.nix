8f9fbec23df4dc67f54f8f1b1e86409f2b586a72
Matthias Beyer <mail@beyermatthias.de>
2019-06-14 17:18:52 +0200

    It seems we dont need this as node2nix takes care of electron

--- shell.nix
+++ shell.nix
@@ -1,19 +1,6 @@
 { pkgs ? (import <nixpkgs> {}) }:
 
 let
-  # we need unstable nixpkgs for electron 5
-  unstable = let
-    rev    = "168d1031af4916354491a1b1dd103258cfd26c0e";
-    sha256 = "1mzir6zlf1lxcl7l1pz82nziwi68gf7zjpwbqrj7sv3vn6jknyxw";
-
-    tarball = builtins.fetchTarball {
-      inherit sha256;
-      url = "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz";
-    };
-  in import tarball {
-    config.allowUnfree = false;
-  };
-
   rust = pkgs.rustChannels.stable.rust.override {
     targets = [ "wasm32-unknown-unknown" ];
   };
@@ -28,10 +15,7 @@ let
     nodePackages.npm
     nodePackages.node2nix
     nodejs
-
-  ] ++ (with unstable; [
-    electron_5
-  ]);
+  ];
 
 in
 
