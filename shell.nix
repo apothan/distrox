a04d5d5c6c11a4626af9263bbad70690df14c8be
Matthias Beyer <mail@beyermatthias.de>
2019-06-14 15:53:44 +0200

    Add shell.nix with rustc for wasm

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? (import <nixpkgs> {}) }:
+
+let
+  rust = pkgs.rustChannels.stable.rust.override {
+    targets = [ "wasm32-unknown-unknown" ];
+  };
+
+  env = with pkgs.rustChannels.stable; [
+    rust
+    cargo
+  ];
+
+  dependencies = with pkgs; [
+  ];
+in
+
+pkgs.stdenv.mkDerivation rec {
+    name = "distrox";
+    src = /var/empty;
+    version = "0.0.0";
+
+    buildInputs = env ++ dependencies;
+
+  shellHook = ''
+    HISTFILE=${toString ./.history}
+  '';
+}
+
