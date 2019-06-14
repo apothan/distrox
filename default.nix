2622a337261a45a044c0acf9e4700c439cef09a6
Matthias Beyer <mail@beyermatthias.de>
2019-06-14 22:17:00 +0200

    Add default.nix for dependencies

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? (import <nixpkgs> {}) }:
+
+let
+  env = with pkgs.rustChannels.stable; [
+    rust
+    cargo
+  ];
+
+  dependencies = with pkgs; [
+    pkgconfig
+    glib
+    cairo
+    atk
+    pango
+    gdk_pixbuf
+    gtk3
+  ];
+in
+
+pkgs.stdenv.mkDerivation rec {
+    name = "distrox";
+    src = /var/empty;
+    version = "0.0.0";
+
+    buildInputs = env ++ dependencies;
+
+}
+
