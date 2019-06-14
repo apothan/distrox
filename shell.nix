8f2be91a99c9f77af9d2b1984a142676d7baa29a
Matthias Beyer <mail@beyermatthias.de>
2019-06-14 17:10:15 +0200

    TMP

--- shell.nix
+++ shell.nix
@@ -1,6 +1,19 @@
 { pkgs ? (import <nixpkgs> {}) }:
 
 let
+  # we need unstable nixpkgs for electron 5
+  unstable = let
+    rev    = "168d1031af4916354491a1b1dd103258cfd26c0e";
+    sha256 = "1mzir6zlf1lxcl7l1pz82nziwi68gf7zjpwbqrj7sv3vn6jknyxw";
+
+    tarball = builtins.fetchTarball {
+      inherit sha256;
+      url = "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz";
+    };
+  in import tarball {
+    config.allowUnfree = false;
+  };
+
   rust = pkgs.rustChannels.stable.rust.override {
     targets = [ "wasm32-unknown-unknown" ];
   };
@@ -11,13 +24,15 @@ let
   ];
 
   dependencies = with pkgs; [
-    openssl
-    pkgconfig
-
     # node
     nodePackages.npm
+    nodePackages.node2nix
     nodejs
-  ];
+
+  ] ++ (with unstable; [
+    electron_5
+  ]);
+
 in
 
 pkgs.stdenv.mkDerivation rec {
